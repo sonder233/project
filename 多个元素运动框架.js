02a9a2b7bcea94d1fb298707a7782ea5206de790
sonder233 <2332447236@qq.com>
2019-01-18 10:43:59 +0800

    完善透明度情况

--- 多个元素运动框架.js
+++ 多个元素运动框架.js
@@ -1,35 +1,51 @@
-var timer=null;
-        window.onload = function () {
-            var mDiv = document.getElementsByTagName('div');
-            mDiv[0].onmouseover = function () {
-                StartMove(this, 'width', 300)
-            }
-            mDiv[1].onmouseover = function () {
-                StartMove(this, 'height', 150)
-            }
-            mDiv[2].onmouseover = function () {
-                StartMove(this, 'fontSize', 100)
-            }
+var timer = null;
+window.onload = function () {
+    var mDiv = document.getElementsByTagName('div');
+    mDiv[0].onmouseover = function () {
+        StartMove(this, 'width', 300)
+    }
+    mDiv[1].onmouseover = function () {
+        StartMove(this, 'height', 150)
+    }
+    mDiv[2].onmouseover = function () {
+        StartMove(this, 'fontSize', 100)
+    }
+    mDiv[3].onmouseover = function () {
+        StartMove(this, 'opacity', 500)
+    }
+}
+function getStyle(obj, attr) {
+    if (obj.currentStyle) {
+        return obj.currentStyle[attr];
+    }
+    else {
+        return getComputedStyle(obj, false)[attr];
+    }
+}
+function StartMove(obj, attr, target) {
+    clearInterval(obj, timer);
+    obj.timer = setInterval(function () {
+        var mCur = 0;
+        if (attr == 'opacity') {
+            mCur = parseInt(parseFloat(getStyle(obj, attr)) * 100);//把不透明度化为整数，因为后面都是按照整数处理的
         }
-        function getStyle(obj, attr) {
-            if (obj.currentStyle) {
-                return obj.currentStyle[attr];
+        else {
+            mCur = parseInt(getStyle(obj, attr));
+        }
+        var mCur = parseInt(getStyle(obj, attr));
+        var mSpeed = (target - mCur) / 8;
+        mSpeed = mSpeed > 0 ? Math.ceil(mSpeed) : Math.floor(mSpeed);
+        if (mCur == target) {
+            clearInterval(obj.timer)
+        }
+        else {
+            if (attr == 'opacity') {
+                obj.style.opacity = (mCur + mSpeed) / 100;
             }
             else {
-                return getComputedStyle(obj, false)[attr];
+                obj.style[attr] = mCur + mSpeed + 'px';
             }
+
         }
-        function StartMove(obj, attr, target) {
-            clearInterval(obj, timer);
-            obj.timer = setInterval(function () {
-                var mCur = parseInt(getStyle(obj, attr));
-                var mSpeed = (target - mCur) / 8;
-                mSpeed = mSpeed > 0 ? Math.ceil(mSpeed) : Math.floor(mSpeed);
-                if (mCur == target) {
-                    clearInterval(obj.timer)
-                }
-                else {
-                    obj.style[attr] = mCur + mSpeed + 'px';
-                }
-            }, 30)
-        }
+    }, 30)
+}
